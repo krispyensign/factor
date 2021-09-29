8dea7b0e3795c528cfb66c04638dd6894e8bb607
Jeremy Futral <jfutral@fanatics.com>
2021-09-29 15:29:12 -0400

    added comments

--- functor.py
+++ functor.py
@@ -26,7 +26,11 @@ class Functor:
             self.f = z
 
     def print(self) -> None:
+        # print and pretty print the main function
         pprint(self.f)
+        print(self.f)
+
+        # print the 4 sub functions
         print(
             Functor(self.f.subs({self.x: 2*self.x, self.y: 2*self.y})).smooth())
         print(
@@ -35,25 +39,36 @@ class Functor:
             Functor(self.f.subs({self.x: 2*self.x, self.y: 2*self.y + 1})).smooth())
         print(
             Functor(self.f.subs({self.x: 2*self.x + 1, self.y: 2*self.y + 1})).smooth())
-        print(self.f)
+        
+        # print the matrix
         matrix_print(self.gen_matrix())
+
+        # print the separator
         sep_print()
 
     def gen_matrix(self, m=4) -> list[list[Integer]]:
+        # generate the m x m (default = 4 x 4) binary matrix
         return [[Integer(Mod(self.f, 2).subs({self.x: i, self.y: j})) for i in range(m)] for j in range(m)]
 
     def lift(self, rotate_x: bool) -> Tuple[Add, bool, bool]:
+        # perform pattern match to get the subexpression and various flags
         do_division, sub_expression, is_rotated = self.matcher(
             self.gen_matrix(), rotate_x=rotate_x)
+        
+        # pretty print the sub expression
         pprint(sub_expression)
 
+        # create a new function with the sub expression and smooth it for faster processing
         f = Functor(Functor(self.f.subs(sub_expression)).smooth()).smooth()
+
+        # if the sub expression is 2*x + w or 2*y + w then divide by two
         if do_division is True:
             f /= 2
 
         return f, do_division, is_rotated
 
     def smooth(self) -> Add:
+        # replace complicated exponents with simpler isomorphic exponents over the integers
         return (self.f
                 .expand()
                 .subs((-1)**(2*self.y), 1)
@@ -80,7 +95,6 @@ class Functor:
         m: list[list[Integer]],
         rotate_x: bool
     ) -> Tuple[bool, dict[Symbol, Add], bool]:
-
         # setup rotation, rotate different direction than last time
         rotator: dict[Symbol, Add]
         if rotate_x is True:
@@ -88,6 +102,7 @@ class Functor:
         else:
             rotator = {self.y: self.x + self.y}
 
+        # perform pattern matching
         if m == [[1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 1, 0]]:
             return True, {self.x: 2*self.x + 1}, False
         elif m == [[0, 1, 0, 1], [0, 1, 0, 1], [0, 1, 0, 1], [0, 1, 0, 1]]:

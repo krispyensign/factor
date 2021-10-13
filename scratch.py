fc403ac26352fb71590e198225099de6b5f3e437
Jeremy Futral <jfutral@fanatics.com>
2021-10-13 09:23:55 -0400

    bumped python and added encode def

--- scratch.py
+++ scratch.py
@@ -1,110 +1,88 @@
 from functor import Functor
 from sympy import (
-    Mod,
     symbols,
-    print_maple_code,
-    pprint,
-    Add,
-    Integer,
     Symbol,
     I,
-    Rational,
-    radsimp,
-    sqrt,
     Matrix,
+    Add,
+    pprint,
+    Mul
 )
 
 from typing import Tuple
 
 from utils import matrix_print, sep_print
 
-if __name__ == "__main__":
+def encode(A: Matrix) -> Add | Mul:
     x: Symbol
     y: Symbol
-    x, y, i, j, k = symbols('x,y,j,k,i')
-
-    def comp_k(xx):
-        return [1, 1, -1, -1][xx % 4]
-
-    def comp_j(xx):
-        return [1, -1, -1, 1][xx % 4]
-
-    def comp_i(xx):
-        return [1, -1, 1, -1][xx % 4]
-
-
-    g1 = 1
-    g2 = (-1)**x
-
-    # print([f.subs({
-    #     k: comp_k(n),
-    #     j: comp_j(n),
-    #     i: comp_i(n),
-    # }) for n in range(4)])
-
-    # kf = I**x - ((1 - (-1)**x)/2)*(I**x)
-    # kf += kf.subs({x: x + 1})
-    # kf = kf.expand().xreplace({
-    #     (-1)**x: I**(2*x),
-    #     I * I**(3*x): I**(3*x + 1)
-    # })
-    # pprint(kf.expand())
-    iff = (-1)**x
-    pprint(iff)
-    print([iff.subs({x: n}) for n in range(4)])
-    sep_print()
-
-    kf = I**(3*x) / 2 - I**(3*x + 1) / 2 + I**x/2 + I**(x+1)/2
-    pprint(kf)
-    print([kf.subs({x: n}) for n in range(4)])
-    sep_print()
-
-    jf = kf.subs({x: x + 3}).subs({
+    x, y = symbols('x, y')
+    # setup walsh functions
+    ii = (-1)**x
+    kk = I**(3*x) / 2 - I**(3*x + 1) / 2 + I**x / 2 + I**(x + 1) / 2
+    jj = kk.subs({x: x + 3}).subs({
         I**(x + 4):  I**x,
-        I**(3*x+9):  I**(3*x + 1),
-        I**(3*x+10): I**(3*x + 2),
+        I**(3*x + 9):  I**(3*x + 1),
+        I**(3*x + 10): I**(3*x + 2),
     })
-    pprint(jf)
-    print([jf.subs({x: n}) for n in range(4)])
-    sep_print()
 
-    Ident = Matrix([
+    # perform hadamard transform
+    B: Matrix = (A * Matrix([
         [1,  1,  1,  1],
         [1, -1,  1, -1],
         [1,  1, -1, -1],
         [1, -1, -1,  1],
-    ])
-    pprint(Ident)
-    sep_print()
+    ])).multiply_elementwise(Matrix([
+        [1, ii, jj, kk]
+    ]))
 
-    M1 = Matrix([
-        [1, iff, jf, kf]
-    ])
-    # pprint(M1)
+    # convert to function
+    return (B[0] + B[1] + B[2] + B[3]).expand().xreplace({
+        (-1)**x: I**(2*x),
+    }).subs({
+        I * I**(3*x): I**(3*x + 1),
+    })
 
-    M2 = Matrix([
-        [2, 3, 1, 2]
-    ])
-    pprint(M2)
+if __name__ == "__main__":
+    x: Symbol
+    y: Symbol
+    a: Symbol
+    b: Symbol
+    c: Symbol
+    d: Symbol
 
-    M3 = M2 * Ident
-    pprint(M3)
-    # 8 -2  2 0 = 8
-    # 8  2  2 0 = 12
-    # 8 -2 -2 0 = 4
-    # 8  2 -2 0 = 8
-    f = (M3[0] + M3[1]*iff + M3[2]*jf + M3[3]*kf) / 4
+    x, y, a, b, c, d = symbols('x, y, a, b, c, d')
+
+    f = encode(Matrix([[a, b, c, d]]))
     pprint(f)
     sep_print()
-
-    print(M3[0])
-    print([(M3[1] * iff).subs({x: n}) for n in range(4)])
-    print([(M3[2] * jf).subs({x: n}) for n in range(4)])
-    print([(M3[3] * kf).subs({x: n}) for n in range(4)])
+    fI1 = encode(Matrix([[I**a, I**b, I**c, I**d]]))
+    pprint(fI1)
+    sep_print()
+    fI2 = encode(Matrix([[I**(2*a), I**(2*b), I**(2*c), I**(2*d)]]))
+    pprint(fI2)
+    sep_print()
+    fI3 = encode(Matrix([[I**(3*a), I**(3*b), I**(3*c), I**(3*d)]]))
+    fI3 = fI3.subs({
+        I**(3*a) * I**(3*x):        I**(3*a + 3*x),
+        I**(3*a) * I**(2*x):        I**(3*a + 2*x),
+        I**(3*a) * I**x:            I**(3*a + x),
+        I**(3*b) * I**(2*x):        I**(3*b + 2*x),
+        I * I**(3*b) * I**(x):      I**(3*b + x + 1),
+        I**(3*b) * I**(3*x + 1):    I**(3*b + 3*x + 1),
+        I**(3*c) * I**(3*x):        I**(3*c + 3*x),
+        I**(3*c) * I**(2*x):        I**(3*c + 2*x),
+        I**(3*c) * I**x:            I**(3*c + x),
+        I**(3*d) * I**(2*x):        I**(3*d + 2*x),
+        I * I**(3*d) * I**x:        I**(3*d + x + 1),
+        I**(3*d) * I**(3*x + 1):    I**(3*d + 3*x + 1),
+    }).xreplace({
+        
+    })
+    pprint(fI3)
     sep_print()
 
-    print([n for n in range(4)])
-    print([f.subs({x: n}) for n in range(4)])
-    print([I**f.subs({x: n}) for n in range(4)])
-    print([I**(2*f.subs({x: n})) for n in range(4)])
-    print([I**(3*f.subs({x: n})) for n in range(4)])
+    print([(fI1).subs({x: n}) for n in range(4)])
+    print([(fI2).subs({x: n}) for n in range(4)])
+    print([(fI3).subs({x: n}) for n in range(4)])
+    sep_print()

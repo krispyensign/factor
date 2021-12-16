45a6ed0345f569b426b3464b4bbf6345078e9d79
KrispyEnsign <futralj@gmail.com>
2021-12-16 01:07:12 -0500

    cleaned up constraints

--- test.v
+++ test.v
@@ -133,7 +133,6 @@ Proof.
   rewrite Z.mul_comm with (m := a) (n := 2).
   unfold i.
   rewrite Z.mod_mul.
-  zify.
   lia.
   discriminate.
 Qed.
@@ -167,16 +166,13 @@ Qed.
 
 
 Theorem Zk_pow_2_r : forall a b,
-  b = 0 \/ b = 1 \/ b = 2 \/ b = 3 ->
+  0 <= b < 4 ->
   (k (4*a + b))^2 = 1.
 Proof.
   intros.
   rewrite Zk_mod_add.
-  destruct H as [b0 | [b1 | [b2 | b3]]].
-  - subst. auto.
-  - subst. auto.
-  - subst. auto.
-  - subst. auto.
+  unfold k. unfold j. unfold i.
+  nia.
 Qed.
 
 
@@ -226,7 +222,7 @@ Qed.
 
 
 Theorem ZW_mod_add_l : forall a b c d u v,
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->
   (W a b c d (4*u + v)) = (W a b c d v).
 Proof.
   intros.
@@ -239,7 +235,7 @@ Qed.
 
 
 Theorem ZW_W_l : forall a b c d u v,
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->
   (W (W a b c d 0)
     (W a b c d 1)
     (W a b c d 2)
@@ -248,18 +244,21 @@ Theorem ZW_W_l : forall a b c d u v,
 Proof.
   intros.
   rewrite ZW_mod_add_l.
-  unfold W. unfold W. unfold k. unfold j. unfold i.
-  destruct H as [H0 | [H1 | [H2 | H3]]].
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
+  replace (W a b c d 0) with a.
+  replace (W a b c d 1) with b.
+  replace (W a b c d 2) with c.
+  replace (W a b c d 3) with d.
+  - reflexivity.
+  - symmetry. rewrite ZW_eq with (w := d). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := c). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := b). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := a). reflexivity. auto.
   - assumption.
 Qed.
 
 
 Theorem ZW_W_add_l : forall a b c d e f g h u v,
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->
   (W  ((W a b c d 0) + e)
     ((W a b c d 1) + f)
     ((W a b c d 2) + g)
@@ -268,12 +267,15 @@ Theorem ZW_W_add_l : forall a b c d e f g h u v,
 Proof.
   intros.
   rewrite ZW_mod_add_l.
-  unfold W. unfold W. unfold k. unfold j. unfold i.
-  destruct H as [H0 | [H1 | [H2 | H3]]].
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
+  replace (W a b c d 0) with a.
+  replace (W a b c d 1) with b.
+  replace (W a b c d 2) with c.
+  replace (W a b c d 3) with d.
+  - reflexivity.
+  - symmetry. rewrite ZW_eq with (w := d). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := c). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := b). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := a). reflexivity. auto.
   - assumption.
 Qed.
 
@@ -298,7 +300,6 @@ Qed.
   For any periodic function f, Wf f is also periodic
 *)
 Theorem ZWf_mod_add_l : forall (f : Z->Z) (u v : Z),
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
   (Wf f (4*u + v)) = (Wf f v).
 Proof.
   intros.
@@ -314,7 +315,6 @@ Qed.
   when f(t,v) is periodic when t or v is held constant.
 *)
 Theorem ZWff_mod_add_l : forall (f : Z->Z->Z) (t u v : Z),
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
   (Wf (f t) (4*u + v)) = (Wf (f t) v).
 Proof.
   intros.
@@ -329,66 +329,65 @@ Qed.
   Proves that repeated application eliminates other applications
 *)
 Theorem ZWf_Wf_l : forall (f : Z->Z) (u v : Z),
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->  
   (Wf (Wf f) (4*u + v)) = (Wf f v).
 Proof.
   intros.
-  rewrite ZWf_mod_add_l.
-  unfold Wf. repeat unfold W. unfold k. unfold j. unfold i.
-  destruct H as [H0 | [H1 | [H2 | H3]]].
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - assumption.
+  unfold Wf. rewrite ZW_W_l.
+  reflexivity.
+  assumption.
 Qed.
 
 (*
   Proves that repeated application eliminates other applications
 *)
 Theorem ZWf_Wff_l : forall (f : Z->Z->Z) (t u v : Z),
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->  
   (Wf (Wf (f t)) (4*u + v)) = (Wf (f t) v).
 Proof.
   intros.
-  rewrite ZWf_mod_add_l.
-  unfold Wf. repeat unfold W. unfold k. unfold j. unfold i.
-  destruct H as [H0 | [H1 | [H2 | H3]]].
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - assumption.
+  unfold Wf. rewrite ZW_W_l.
+  reflexivity.
+  assumption.
 Qed.
 
 Theorem ZWf_Wf_add_l : forall (g f : Z->Z) (u v : Z),
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->  
   (Wf (zip g (Wf f)) (4*u + v)) = (Wf (zip g f) v).
 Proof.
   intros.
-  rewrite ZWf_mod_add_l.
-  unfold zip. unfold Wf. repeat unfold W. unfold k. unfold j. unfold i.
-  destruct H as [H0 | [H1 | [H2 | H3]]].
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
+  unfold zip. unfold Wf. 
+  replace (W (f 0) (f 1) (f 2) (f 3) 0) with (f 0).
+  replace (W (f 0) (f 1) (f 2) (f 3) 1) with (f 1).
+  replace (W (f 0) (f 1) (f 2) (f 3) 2) with (f 2).
+  replace (W (f 0) (f 1) (f 2) (f 3) 3) with (f 3).
+  rewrite ZW_mod_add_l.
+  - reflexivity.
   - assumption.
+  - symmetry. rewrite ZW_eq with (w := (f 3)). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := (f 2)). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := (f 1)). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := (f 0)). reflexivity. auto.
 Qed.
 
+
 Theorem ZWf_Wff_add_l : forall (g f : Z->Z->Z) (t u v : Z),
-  v = 0 \/ v = 1 \/ v = 2 \/ v = 3 ->
+  0 <= v < 4 ->  
   (Wf (zip (g t) (Wf (f t))) (4*u + v)) = (Wf (zip (g t) (f t)) v).
 Proof.
   intros.
-  rewrite ZWf_mod_add_l.
-  unfold zip. unfold Wf. repeat unfold W. unfold k. unfold j. unfold i.
-  destruct H as [H0 | [H1 | [H2 | H3]]].
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
-  - subst. simpl. lia.
+  unfold zip. unfold Wf.
+  replace (W (f t 0) (f t 1) (f t 2) (f t 3) 0) with (f t 0).
+  replace (W (f t 0) (f t 1) (f t 2) (f t 3) 1) with (f t 1).
+  replace (W (f t 0) (f t 1) (f t 2) (f t 3) 2) with (f t 2).
+  replace (W (f t 0) (f t 1) (f t 2) (f t 3) 3) with (f t 3).
+  rewrite ZW_mod_add_l.
+  - reflexivity.
   - assumption.
+  - symmetry. rewrite ZW_eq with (w := (f t 3)). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := (f t 2)). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := (f t 1)). reflexivity. auto.
+  - symmetry. rewrite ZW_eq with (w := (f t 0)). reflexivity. auto.
 Qed.
 
 

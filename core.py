d59ea93e557a421917ae940e037e99a691fe9fca
Jeremy Futral <jfutral@fanatics.com>
2021-12-20 17:03:41 -0500

    start of cleanup

--- core.py
+++ core.py
@@ -12,7 +12,7 @@ from sympy import (  # type: ignore
 )
 
 
-class i(Function):
+class w1(Function):
     """Walsh[1] function"""
     @classmethod
     def eval(cls, n: int | Expr) -> int | Function:
@@ -24,7 +24,7 @@ class i(Function):
                 case 3: return -1
 
 
-class j(Function):
+class w2(Function):
     """Walsh[2] function"""
     @classmethod
     def eval(cls, n: int | Expr) -> int | Function:
@@ -36,7 +36,7 @@ class j(Function):
                 case 3: return -1
 
 
-class k(Function):
+class w3(Function):
     """Walsh[3] function"""
     @classmethod
     def eval(cls, n: int | Expr) -> int | Function:
@@ -51,7 +51,6 @@ class k(Function):
 # define some global symbols
 x: Symbol = symbols('x')
 y: Symbol = symbols('y')
-n: list[Symbol] = symbols(','.join([f"n{ii}" for ii in range(29)]))
 a: list[Symbol] = symbols(','.join([f"a{ii}" for ii in range(18)]))
 b: list[Symbol] = symbols(','.join([f"b{ii}" for ii in range(18)]))
 c: list[Symbol] = symbols('c0, c1, c2, c3')
@@ -80,18 +79,18 @@ def condense_terms_c(f: Add) -> Add:
 def walsh_reduction(f: Add) -> Add:
     # perform reduction of walsh function args
     return f.expand().subs({
-        i(y)**2: 1,
-        i(x)**2: 1,
-        j(x)**2: 1,
-        j(y)**2: 1,
-        k(x)**2: 1,
-        k(y)**2: 1,
-        i(2*y): 1,
-        i(2*x): 1,
-        j(2*x): i(x),
-        j(2*y): i(y),
-        k(2*x): i(x),
-        k(2*y): i(y),
+        w1(y)**2: 1,
+        w1(x)**2: 1,
+        w2(x)**2: 1,
+        w2(y)**2: 1,
+        w3(x)**2: 1,
+        w3(y)**2: 1,
+        w1(2*y): 1,
+        w1(2*x): 1,
+        w2(2*x): w1(x),
+        w2(2*y): w1(y),
+        w3(2*x): w1(x),
+        w3(2*y): w1(y),
     })
 
 
@@ -107,66 +106,9 @@ def hadamard_transform(A: Matrix, v: Symbol) -> Add | Mul:
     # convert to function
     return Rational(1/4)*(
         B[0] +
-        B[1]*i(v) +
-        B[2]*j(v) +
-        B[3]*k(v)
-    )
-
-
-def create_generalized_polynomial() -> Add:
-    # define the core function
-    return (
-        n[0]*x**2 +
-        n[1]*x*y +
-        n[2]*y**2 +
-
-        x*(
-            n[3] +
-
-            n[4]*i(a[0]*x) +
-            n[5]*i(b[0]*y) +
-            n[6]*i(a[1]*x + b[1]*y) +
-
-            n[6]*j(a[2]*x) +
-            n[7]*j(b[2]*y) +
-            n[8]*j(a[3]*x + b[3]*y) +
-
-            n[6]*k(a[4]*x) +
-            n[7]*k(b[4]*y) +
-            n[8]*k(a[5]*x + b[5]*y)
-        ) +
-
-        y*(
-            n[9] +
-
-            n[10]*i(a[6]*x) +
-            n[11]*i(b[6]*y) +
-            n[12]*i(a[7]*x + b[7]*y) +
-
-            n[13]*j(a[8]*x) +
-            n[14]*j(b[8]*y) +
-            n[15]*j(a[9]*x + b[9]*y) +
-
-            n[16]*k(a[10]*x) +
-            n[17]*k(b[10]*y) +
-            n[18]*k(a[11]*x + b[11]*y)
-        ) +
-
-        (
-            n[19] +
-
-            n[20]*i(a[12]*x) +
-            n[21]*i(b[12]*y) +
-            n[22]*i(a[13]*x + b[13]*y) +
-
-            n[23]*j(a[14]*x) +
-            n[24]*j(b[14]*y) +
-            n[25]*j(a[15]*x + b[15]*y) +
-
-            n[26]*k(a[16]*x) +
-            n[27]*k(b[16]*y) +
-            n[28]*k(a[17]*x + b[17]*y)
-        )
+        B[1]*w1(v) +
+        B[2]*w2(v) +
+        B[3]*w3(v)
     )
 
 
@@ -191,12 +133,12 @@ def shift_polynomial(f: Add, v: Symbol, w: Symbol, shift: Add) -> Add:
     # TODO: add more replacers for encoding.
     # encode everything to reduce to simpler form
     for s in [a, b]:
-        for ii in range(18):
+        for n in range(18):
             # substitute with encoding
             g = g.xreplace({
-                i(s[ii]*q): encode(s[ii], shift, w, i),
-                j(s[ii]*q): encode(s[ii], shift, w, j),
-                k(s[ii]*q): encode(s[ii], shift, w, k),
+                w1(s[n]*q): encode(s[n], shift, w, w1),
+                w2(s[n]*q): encode(s[n], shift, w, w2),
+                w3(s[n]*q): encode(s[n], shift, w, w3),
             })
 
     return walsh_reduction(g.subs({
--- tests.py
+++ tests.py
@@ -3,33 +3,33 @@ import unittest
 from core import *
 
 class TestTransforms(unittest.TestCase):
-    def test_prove_walsh_reduction_i(self):
+    def test_prove_walsh_reduction_w1(self):
         # prove that i**(2*x) == 1
-        source = Matrix(([[i(2*ii) for ii in range(4)]]))
-        target = Matrix(([[1 for ii in range(4)]]))
+        source = Matrix(([[w1(2*n) for n in range(4)]]))
+        target = Matrix(([[1 for n in range(4)]]))
         self.assertTrue(source.equals(target))
 
-    def test_prove_walsh_reduction_j(self):
+    def test_prove_walsh_reduction_w2(self):
         # prove that j**(2*x) == i**x
-        source = Matrix(([[j(2*ii) for ii in range(4)]]))
-        target = Matrix(([[i(ii) for ii in range(4)]]))
+        source = Matrix(([[w2(2*n) for n in range(4)]]))
+        target = Matrix(([[w1(n) for n in range(4)]]))
         self.assertTrue(source.equals(target))
 
-    def test_prove_walsh_reduction_k(self):
+    def test_prove_walsh_reduction_w3(self):
         # prove that k**(2*x) == i**x
-        source = Matrix(([[k(2*ii) for ii in range(4)]]))
-        target = Matrix(([[i(ii) for ii in range(4)]]))
+        source = Matrix(([[w2(2*n) for n in range(4)]]))
+        target = Matrix(([[w1(n) for n in range(4)]]))
         self.assertTrue(source.equals(target))
 
     def test_prove_transform(self):
         # prove the transform
-        source = hadamard_transform(Matrix([[c[ii] for ii in range(4)]]), x)
+        source = hadamard_transform(Matrix([[c[n] for n in range(4)]]), x)
 
         # evaluate 0 .. 4 for all cases
-        evaluated = Matrix([[(source.subs({x: ii}).expand()) for ii in range(4)]])
+        evaluated = Matrix([[(source.subs({x: n}).expand()) for n in range(4)]])
 
         # provide expected result
-        target = Matrix([[c[ii] for ii in range(4)]])
+        target = Matrix([[c[n] for n in range(4)]])
         self.assertTrue(evaluated.equals(target))
 
     def test_prove_condense_terms(self):
@@ -37,31 +37,31 @@ class TestTransforms(unittest.TestCase):
         f: Add = create_generalized_shift(x)
 
         # prove condensed terms of a function before a transform still gives original values
-        source = hadamard_transform(Matrix([[condense_terms_d((i(f.subs({
-            x: ii
-        })))) for ii in range(4)]]), x)
+        source = hadamard_transform(Matrix([[condense_terms_d((w1(f.subs({
+            x: n
+        })))) for n in range(4)]]), x)
 
         # evaluate 0 .. 4 for all cases
-        evaluated = Matrix([[(source.subs({x: ii}).expand()) for ii in range(4)]])
+        evaluated = Matrix([[(source.subs({x: n}).expand()) for n in range(4)]])
 
         # provide expected results for all cases
-        target = condense_terms_d(Matrix([[i(f.subs({x: ii})) for ii in range(4)]]))
+        target = condense_terms_d(Matrix([[w1(f.subs({x: n})) for n in range(4)]]))
 
         self.assertTrue(evaluated.equals(target))
 
     def test_prove_encode(self):
         # prove encoded function is equivalent to unencoded function
-        source = encode(a[0], create_generalized_shift(x), x, i)
+        source = encode(a[0], create_generalized_shift(x), x, w1)
 
         # evaluate 0 .. 4 for all cases
-        evaluated = Matrix([[(source.subs({x: ii,})) for ii in range(4)]])
+        evaluated = Matrix([[(source.subs({x: n,})) for n in range(4)]])
 
         # provide expected results 0 .. 4 for all cases with no encoding
-        target = Matrix([[condense_terms_d(((i(a[0]*x)).subs({
+        target = Matrix([[condense_terms_d(((w1(a[0]*x)).subs({
             x: create_generalized_shift(x),
         }).subs({
-            x: ii,
-        }))) for ii in range(4)]])
+            x: n,
+        }))) for n in range(4)]])
 
         self.assertTrue(evaluated.equals(target))
 
